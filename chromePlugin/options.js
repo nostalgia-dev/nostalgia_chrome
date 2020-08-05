681478339c62a675102a5f15e79ce90375957d07
André Jaenisch <andre.jaenisch@posteo.de>
2020-08-05 20:27:54 +0200

    Wire buttons with data model
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- chromePlugin/options.js
+++ chromePlugin/options.js
@@ -1,5 +1,47 @@
 (function() {
     'use strict';
+    const initialData = {
+        "nostalgia-server": "http://localhost:21487/",
+        "downtime-ms": 1000
+    };
+
+    function updateForm(data) {
+        'use strict';
+        Object.keys(data).forEach((key) => {
+            var el = document.getElementById(key);
+            el.value = data[key];
+        });
+
+        var form = document.querySelector('form');
+        form.addEventListener('submit', onSubmit);
+
+        var reset = document.getElementById('reset-defaults');
+        reset.addEventListener('click', onResetDefaults);
+    }
+
+    function onSubmit(event) {
+        'use strict';
+        event.preventDefault();
+        var form = event.target;
+        var formData = new FormData(form);
+
+        // Ensure URL is properly formatted
+        var url = new URL(formData.get('nostalgia-server'));
+        formData.set('nostalgia-server', url.toString());
+
+        var data = {};
+        for (var pair of formData.entries()) {
+            data[pair[0]] = pair[1];
+        }
+        return updateStorage(data);
+    }
+
+    async function onResetDefaults() {
+        'use strict';
+        updateForm(initialData);
+        return new Promise((resolve) => chrome.storage.local.set(initialData, resolve));
+    }
+    
     function updateStorage(data) {
         'use strict';
         return new Promise((resolve) => chrome.storage.local.set(data, resolve));
@@ -10,20 +52,10 @@
         return new Promise((resolve) => chrome.storage.local.get(keys, resolve));
     }
 
-    const data = {
-        "nostalgia-server": "http://localhost:21487/",
-        "downtime-ms": 1000
-    };
-
     async function run () {
         'use strict';
-        var logger = getLogger('run');
-        var unknown = await readStorage(['unknown']);
-        logger.debug('What about', unknown);
-        await updateStorage(data);
-        logger.debug('Updated storage');
-        var read = await readStorage(['nostalgia-server']);
-        logger.debug('Read', read);
+        var data = await readStorage(Object.keys(initialData));
+        return updateForm(data);
     }
 
     run();
